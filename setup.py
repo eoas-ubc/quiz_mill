ee577c814687e790832f7aa3e01ba7ef4083118c
Phil Austin <paustin@eos.ubc.ca>
2021-06-15 12:54:18 -0700

    add versioning

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 
-from setuptools import setup
-
+from setuptools import setup, find_packages
+from setuptools_scm import get_version
 
 setup(
     name='quiz_mill',
-    packages=['quiz_mill'],
+    version=get_version(),
+    packages=find_packages(include=['quiz_mill']),
     classifiers=[
         'License :: OSI Approved :: BSD License'
     ],
-    install_requires=[
-        "papermill","click"
-    ],
     entry_points={
           'console_scripts': [
-              'run-papermill = quiz_mill.run_papermill:main',
-              'filter-notebook = quiz_mill.filter_notebook:main'
+-              'run-papermill = quiz_mill.run_papermill:main',
+-              'filter-notebook = quiz_mill.filter_notebook:main'
           ]
     },
-    long_description="""description""")
+    keywords='eoas ubc ocese',
+    long_description="""canvas quiz creation""")
