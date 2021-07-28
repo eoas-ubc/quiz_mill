99d7c714cdc1218b09a3ec36d9d2e46f88abe0d5
Carol Zhang <zhang.carol19@gmail.com>
2021-07-28 12:17:29 -0700

    command now requires path as input

--- quiz_mill/generate_notebooks.py
+++ quiz_mill/generate_notebooks.py
@@ -1,14 +1,18 @@
 import papermill as pm
 import random
 import click
+from pathlib import Path
 
 # Generates specified number of two_layers.ipynb notebooks with random parameters
 # Example command to generate 5 notebooks with random parameters:
 #   python generate_notebooks.py -n 5
 
 @click.command()
+@click.argument("path", type=str, nargs=1)
 @click.option("-n", "--number")
-def main(number):
+def main(path, number):
+    path = Path(path).resolve()
+    print(path)
     for i in range(int(number)):
         sol =       round(random.uniform(0.0, 500.0), 1)
         epsilon1 =  round(random.uniform(0.0, 1.0), 2)
@@ -16,8 +20,8 @@ def main(number):
         albedo =    round(random.uniform(0.0, 1.0), 2)
 
         pm.execute_notebook(
-            '../notebooks/two_layers.ipynb',
-            '../notebooks/output/unfiltered/output_two_layers{}.ipynb'.format(i+1),
+            path / "two_layers.ipynb",
+            path / "output/unfiltered/output_two_layers{}.ipynb".format(i+1),
             parameters=dict(sol=sol, epsilon1=epsilon1, epsilon2=epsilon1, albedo=albedo)
         )
 
